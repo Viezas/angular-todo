9e488afc6ba018fa8362aaf8ede35b48d9ff1cb6
tony <tony_osei@hotmail.com>
2024-04-13 22:07:13 +0200

    ajout du task / css

---
+++ src/app/components/new-task/new-task.component.ts
@@ -0,0 +1,107 @@
+import { Component } from '@angular/core';
+import { CardComponent } from '../card/card.component';
+import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { TaskService } from '../../service/task/task.service';
+import { AuthService } from '../../service/auth/auth.service';
+import { collection, getDocs, onSnapshot } from '@angular/fire/firestore';
+import { TaskInterface } from '../../interface/task';
+
+@Component({
+  selector: 'app-new-task',
+  standalone: true,
+  imports: [CardComponent, CommonModule, FormsModule, ReactiveFormsModule],
+  templateUrl: './new-task.component.html',
+  styleUrl: './new-task.component.css'
+})
+export class NewTaskComponent {
+  constructor(
+    private formBuilder: FormBuilder,
+    private taskService: TaskService,
+    private authService: AuthService
+  ) {}
+
+  submitted: boolean = false;
+  tasks: any[] = [];
+
+  form: FormGroup = new FormGroup({
+    title: new FormControl(''),
+    description: new FormControl(''),
+  });
+
+  async ngOnInit() {
+    this.form = this.formBuilder.group({
+      title: ['', [Validators.required, Validators.minLength(1)]],
+      description: ['', [Validators.nullValidator]],
+    });
+
+    this.authService.user.subscribe(async (user) => {
+      if (user) {
+        await onSnapshot(
+          collection(this.taskService.firestore, 'users', user.uid, 'notes'),
+          (doc) => {
+            this.tasks = [];
+            doc.forEach((doc) => {
+              const task = { ...doc.data(), id: doc.id, is_editing: false };
+              this.tasks.push(task);
+            });
+            this.tasks = this.tasks.sort((a: any, b: any) => a.isDon - b.isDon);
+          }
+        );
+      }
+    });
+  }
+
+  addTask() {
+    const { title, description } = this.form.getRawValue();
+    this.submitted = true;
+
+    if (this.form.valid) {
+      this.taskService.add(title, description);
+      this.resetForm();
+      this.submitted = false;
+    }
+  }
+
+  toggleStatus(task: any) {
+    task.isDon = !task.isDon;
+    this.taskService.update(task);
+  }
+
+  editTask(task: any) {
+    const openedTasks = this.tasks.filter((task) => task.is_editing);
+    openedTasks.forEach((task) => {
+      task.is_editing = false;
+    });
+    task.is_editing = true;
+  }
+
+  updateTask(task: any) {
+    if (task.is_editing) {
+      this.taskService.update(task);
+    }
+  }
+
+  deleteTask(task: any) {
+    if (
+      confirm(
+        `Vous êtes sur le point de supprimer la tâche : "${task.title}". Êtes-vous sûre ?`
+      )
+    ) {
+      this.taskService.delete(task);
+    }
+  }
+
+  resetForm() {
+    this.form.get('title')?.setValue('');
+    this.form.get('description')?.setValue('');
+  }
+}
+
---
+++ src/app/interface/task.ts
@@ -0,0 +1,8 @@
+export interface TaskInterface {
+  id?: string;
+  title: string;
+  subtitle: string;
+  isDon: boolean;
+  time: string;
+  image: number;
+}
