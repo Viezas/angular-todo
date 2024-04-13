56b6ee8f8c6977a51f8f7240a299f97e085c8cdd
Mathis TALBI <talbi.mathis@gmail.com>
2024-04-13 17:18:49 +0200

    login register et connexion firebase

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import {
+  canActivate,
+  redirectLoggedInTo,
+  redirectUnauthorizedTo,
+} from '@angular/fire/auth-guard';
 
-export const routes: Routes = [];
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
+const redirectLoggedInToHome = () => redirectLoggedInTo(['']);
+
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+    ...canActivate(redirectLoggedInToHome),
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    ...canActivate(redirectLoggedInToHome),
+  },
+];
